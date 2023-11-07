ec33e38b97894f81b7639ddae39d0ed9377df846
9R2by <37716834+9R2by@users.noreply.github.com>
2023-11-07 15:25:36 +0100

    update build scripts

---
+++ main.asm
@@ -0,0 +1,698 @@
+	.text
+	.intel_syntax noprefix
+	.file	"main.c"
+	.section	.rodata.cst16,"aM",@progbits,16
+	.p2align	4, 0x0                          # -- Begin function main
+.LCPI0_0:
+	.long	38367248                        # 0x2497010
+	.long	424639921                       # 0x194f7db1
+	.zero	4
+	.zero	4
+.LCPI0_1:
+	.long	3805140                         # 0x3a0fd4
+	.long	38367248                        # 0x2497010
+	.zero	4
+	.zero	4
+.LCPI0_3:
+	.long	301845425                       # 0x11fdcbb1
+	.long	2660819144                      # 0x9e98e0c8
+	.zero	4
+	.zero	4
+.LCPI0_4:
+	.long	2791196368                      # 0xa65e46d0
+	.long	2660819144                      # 0x9e98e0c8
+	.zero	4
+	.zero	4
+.LCPI0_5:
+	.long	1835411675                      # 0x6d6628db
+	.long	414544079                       # 0x18b570cf
+	.zero	4
+	.zero	4
+.LCPI0_6:
+	.long	29                              # 0x1d
+	.long	27                              # 0x1b
+	.zero	4
+	.zero	4
+.LCPI0_7:
+	.long	1410299633                      # 0x540f76f1
+	.long	1835411675                      # 0x6d6628db
+	.zero	4
+	.zero	4
+.LCPI0_8:
+	.long	3670654231                      # 0xdac9bd17
+	.long	4077878197                      # 0xf30f7bb5
+	.zero	4
+	.zero	4
+.LCPI0_10:
+	.long	3670654231                      # 0xdac9bd17
+	.long	3441348404                      # 0xcd1ecf34
+	.zero	4
+	.zero	4
+.LCPI0_11:
+	.long	5                               # 0x5
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+.LCPI0_12:
+	.long	1947338289                      # 0x74120631
+	.long	3670654231                      # 0xdac9bd17
+	.zero	4
+	.zero	4
+.LCPI0_13:
+	.long	589730400                       # 0x23269260
+	.long	3325543546                      # 0xc637c47a
+	.zero	4
+	.zero	4
+.LCPI0_14:
+	.long	1713457115                      # 0x662147db
+	.long	1179748658                      # 0x46518932
+	.zero	4
+	.zero	4
+.LCPI0_15:
+	.long	1                               # 0x1
+	.long	4                               # 0x4
+	.long	1                               # 0x1
+	.long	5                               # 0x5
+.LCPI0_17:
+	.long	3839709255                      # 0xe4dd5047
+	.long	3787625757                      # 0xe1c2951d
+	.zero	4
+	.zero	4
+.LCPI0_19:
+	.long	3136873930                      # 0xbaf8e5ca
+	.long	264758705                       # 0xfc7e5b1
+	.zero	4
+	.zero	4
+.LCPI0_21:
+	.long	3995257464                      # 0xee22ca78
+	.long	1701018484                      # 0x65637b74
+	.zero	4
+	.zero	4
+.LCPI0_22:
+	.long	2322573491                      # 0x8a6fa8b3
+	.long	3995257464                      # 0xee22ca78
+	.zero	4
+	.zero	4
+.LCPI0_24:
+	.long	2244341906                      # 0x85c5f092
+	.long	1874071550                      # 0x6fb40ffe
+	.zero	4
+	.zero	4
+.LCPI0_25:
+	.long	2462883621                      # 0x92cc9f25
+	.long	1428338700                      # 0x5522b80c
+	.zero	4
+	.zero	4
+.LCPI0_26:
+	.long	1874071550                      # 0x6fb40ffe
+	.long	3995257464                      # 0xee22ca78
+	.zero	4
+	.zero	4
+.LCPI0_27:
+	.long	3860553163                      # 0xe61b5dcb
+	.long	2316904350                      # 0x8a19279e
+	.zero	4
+	.zero	4
+.LCPI0_28:
+	.long	3405826175                      # 0xcb00c87f
+	.long	1874071550                      # 0x6fb40ffe
+	.zero	4
+	.zero	4
+.LCPI0_29:
+	.long	1701329325                      # 0x656839ad
+	.long	2648594508                      # 0x9dde584c
+	.zero	4
+	.zero	4
+.LCPI0_30:
+	.long	4146651766                      # 0xf728e276
+	.long	3405826175                      # 0xcb00c87f
+	.zero	4
+	.zero	4
+.LCPI0_31:
+	.long	448733581                       # 0x1abf218d
+	.long	1335952486                      # 0x4fa10466
+	.zero	4
+	.zero	4
+.LCPI0_32:
+	.long	1371361311                      # 0x51bd501f
+	.long	4146651766                      # 0xf728e276
+	.zero	4
+	.zero	4
+.LCPI0_33:
+	.long	1371361311                      # 0x51bd501f
+	.long	481562030                       # 0x1cb40dae
+	.zero	4
+	.zero	4
+.LCPI0_34:
+	.long	2454769752                      # 0x9250d058
+	.long	1371361311                      # 0x51bd501f
+	.zero	4
+	.zero	4
+.LCPI0_35:
+	.long	1746673789                      # 0x681c207d
+	.long	2009482466                      # 0x77c644e2
+	.zero	4
+	.zero	4
+.LCPI0_36:
+	.long	568138029                       # 0x21dd192d
+	.long	3401558597                      # 0xcabfaa45
+	.zero	4
+	.zero	4
+.LCPI0_38:
+	.long	3732587378                      # 0xde7ac372
+	.long	271785344                       # 0x10331d80
+	.zero	4
+	.zero	4
+.LCPI0_40:
+	.long	4214932436                      # 0xfb3ac3d4
+	.long	795203747                       # 0x2f65d8a3
+	.zero	4
+	.zero	4
+.LCPI0_42:
+	.long	643757934                       # 0x265ef76e
+	.long	643231002                       # 0x2656ed1a
+	.zero	4
+	.zero	4
+.LCPI0_43:
+	.long	1337104484                      # 0x4fb29864
+	.long	643757934                       # 0x265ef76e
+	.zero	4
+	.zero	4
+.LCPI0_44:
+	.long	3362392317                      # 0xc86a08fd
+	.long	3501455983                      # 0xd0b3fa6f
+	.zero	4
+	.zero	4
+.LCPI0_45:
+	.long	2825178025                      # 0xa864cba9
+	.long	660734087                       # 0x27620087
+	.zero	4
+	.zero	4
+.LCPI0_46:
+	.long	3501455983                      # 0xd0b3fa6f
+	.long	643757934                       # 0x265ef76e
+	.zero	4
+	.zero	4
+.LCPI0_47:
+	.long	1217297092                      # 0x488e7ac4
+	.long	578521324                       # 0x227b88ec
+	.zero	4
+	.zero	4
+.LCPI0_48:
+	.long	3857211331                      # 0xe5e85fc3
+	.long	3501455983                      # 0xd0b3fa6f
+	.zero	4
+	.zero	4
+.LCPI0_49:
+	.long	3988299470                      # 0xedb89ece
+	.long	4043318113                      # 0xf1002361
+	.zero	4
+	.zero	4
+.LCPI0_50:
+	.long	1373730486                      # 0x51e176b6
+	.long	3857211331                      # 0xe5e85fc3
+	.zero	4
+	.zero	4
+.LCPI0_51:
+	.long	3065303366                      # 0xb6b4d146
+	.long	3327399381                      # 0xc65415d5
+	.zero	4
+	.zero	4
+.LCPI0_52:
+	.long	88614219                        # 0x548254b
+	.long	1373730486                      # 0x51e176b6
+	.zero	4
+	.zero	4
+.LCPI0_53:
+	.long	2607790492                      # 0x9b6fb99c
+	.long	2380920770                      # 0x8de9f7c2
+	.zero	4
+	.zero	4
+.LCPI0_55:
+	.long	3058292004                      # 0xb649d524
+	.long	100997698                       # 0x6051a42
+	.zero	4
+	.zero	4
+.LCPI0_56:
+	.long	1918366004                      # 0x7257f134
+	.long	918351617                       # 0x36bcef01
+	.zero	4
+	.zero	4
+.LCPI0_58:
+	.long	2772499203                      # 0xa540fb03
+	.long	4157083053                      # 0xf7c80dad
+	.zero	4
+	.zero	4
+.LCPI0_60:
+	.long	2411652933                      # 0x8fbee745
+	.long	2424048224                      # 0x907c0a60
+	.zero	4
+	.zero	4
+.LCPI0_62:
+	.long	200715706                       # 0xbf6adba
+	.long	3748201117                      # 0xdf69029d
+	.zero	4
+	.zero	4
+	.section	.rodata.cst8,"aM",@progbits,8
+	.p2align	3, 0x0
+.LCPI0_2:
+	.quad	115964117001                    # 0x1b00000009
+.LCPI0_9:
+	.quad	38654705693                     # 0x90000001d
+.LCPI0_23:
+	.quad	124554051611                    # 0x1d0000001b
+	.section	.rodata.cst4,"aM",@progbits,4
+	.p2align	2, 0x0
+.LCPI0_16:
+	.long	3325543546                      # 0xc637c47a
+.LCPI0_18:
+	.long	4136904798                      # 0xf694285e
+.LCPI0_20:
+	.long	1049849047                      # 0x3e936cd7
+.LCPI0_37:
+	.long	2009482466                      # 0x77c644e2
+.LCPI0_39:
+	.long	2486611920                      # 0x9436afd0
+.LCPI0_41:
+	.long	2480891406                      # 0x93df660e
+.LCPI0_54:
+	.long	88614219                        # 0x548254b
+.LCPI0_57:
+	.long	100997698                       # 0x6051a42
+.LCPI0_59:
+	.long	884053734                       # 0x34b196e6
+.LCPI0_61:
+	.long	3480502563                      # 0xcf744123
+	.text
+	.globl	main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	push	r15
+	.cfi_def_cfa_offset 24
+	push	r14
+	.cfi_def_cfa_offset 32
+	push	r13
+	.cfi_def_cfa_offset 40
+	push	r12
+	.cfi_def_cfa_offset 48
+	push	rbx
+	.cfi_def_cfa_offset 56
+	push	rax
+	.cfi_def_cfa_offset 64
+	.cfi_offset rbx, -56
+	.cfi_offset r12, -48
+	.cfi_offset r13, -40
+	.cfi_offset r14, -32
+	.cfi_offset r15, -24
+	.cfi_offset rbp, -16
+	mov	r13, rsi
+	mov	rdi, qword ptr [rsi + 8]
+	xor	ebx, ebx
+	xor	esi, esi
+	mov	edx, 10
+	call	strtoul
+	mov	r14, rax
+	mov	rdi, qword ptr [r13 + 16]
+	xor	esi, esi
+	mov	edx, 10
+	call	strtoul
+	mov	r15, rax
+	mov	rdi, qword ptr [r13 + 24]
+	xor	esi, esi
+	mov	edx, 10
+	call	strtoul
+	mov	r12, rax
+	mov	rdi, qword ptr [r13 + 32]
+	xor	esi, esi
+	mov	edx, 10
+	call	strtoul
+	mfence
+	mov	rdi, rax
+	rdtscp
+	mov	rsi, rdx
+	lfence
+	bswap	r14d
+	bswap	r15d
+	bswap	r12d
+	bswap	edi
+	mov	ecx, r12d
+	xor	ecx, 151849091
+	xor	edi, 38367248
+	add	edi, ecx
+	vpbroadcastd	xmm0, r15d
+	vpxor	xmm1, xmm0, xmmword ptr [rip + .LCPI0_0]
+	vmovd	xmm0, r14d
+	xor	r14d, -1634148152
+	vpinsrd	xmm0, xmm0, r12d, 1
+	vpxor	xmm2, xmm0, xmmword ptr [rip + .LCPI0_1]
+	vpbroadcastq	xmm0, qword ptr [rip + .LCPI0_2] # xmm0 = [115964117001,115964117001]
+	vpaddd	xmm1, xmm2, xmm1
+	vprolvd	xmm1, xmm1, xmm0
+	rorx	ecx, edi, 3
+	vpxor	xmm2, xmm1, xmmword ptr [rip + .LCPI0_3]
+	vpshufd	xmm3, xmm2, 85                  # xmm3 = xmm2[1,1,1,1]
+	vpaddd	xmm2, xmm3, xmm2
+	vmovd	edx, xmm2
+	mov	edi, ecx
+	xor	edi, -1648018823
+	add	edi, r14d
+	rorx	edi, edi, 3
+	vmovd	xmm2, edi
+	vpinsrd	xmm2, xmm2, ecx, 1
+	vpxor	xmm2, xmm2, xmmword ptr [rip + .LCPI0_4]
+	vpxor	xmm1, xmm1, xmmword ptr [rip + .LCPI0_5]
+	rorx	ecx, edx, 23
+	vpaddd	xmm1, xmm2, xmm1
+	vprolvd	xmm2, xmm1, xmmword ptr [rip + .LCPI0_6]
+	vpextrd	edx, xmm2, 1
+	xor	edx, -1219600979
+	xor	edi, 1835411675
+	add	edi, edx
+	rorx	edx, edi, 5
+	vpxor	xmm1, xmm2, xmmword ptr [rip + .LCPI0_7]
+	vpbroadcastd	xmm3, ecx
+	vpxor	xmm3, xmm3, xmmword ptr [rip + .LCPI0_8]
+	vpaddd	xmm3, xmm1, xmm3
+	vpbroadcastq	xmm1, qword ptr [rip + .LCPI0_9] # xmm1 = [38654705693,38654705693]
+	vprolvd	xmm4, xmm3, xmm1
+	vpbroadcastd	xmm3, edx
+	vpxor	xmm5, xmm3, xmmword ptr [rip + .LCPI0_10]
+	vmovdqa	xmm3, xmmword ptr [rip + .LCPI0_11] # xmm3 = [5,0,2,3]
+	vpermt2d	xmm2, xmm3, xmm4
+	vpxor	xmm2, xmm2, xmmword ptr [rip + .LCPI0_12]
+	vpaddd	xmm2, xmm5, xmm2
+	vpxor	xmm5, xmm4, xmmword ptr [rip + .LCPI0_13]
+	vpshufd	xmm6, xmm5, 85                  # xmm6 = xmm5[1,1,1,1]
+	vpaddd	xmm5, xmm5, xmm6
+	vmovd	ecx, xmm5
+	vprolvd	xmm5, xmm2, xmm0
+	vpxor	xmm6, xmm5, xmmword ptr [rip + .LCPI0_14]
+	vmovdqa	xmm2, xmmword ptr [rip + .LCPI0_15] # xmm2 = [1,4,1,5]
+	vmovd	edx, xmm5
+	vpermt2d	xmm5, xmm2, xmm4
+	vpxord	xmm4, xmm5, dword ptr [rip + .LCPI0_16]{1to4}
+	rorx	ecx, ecx, 3
+	vpaddd	xmm4, xmm6, xmm4
+	vprolvd	xmm4, xmm4, xmm0
+	vpxor	xmm5, xmm4, xmmword ptr [rip + .LCPI0_17]
+	vpshufd	xmm6, xmm4, 85                  # xmm6 = xmm4[1,1,1,1]
+	vpinsrd	xmm6, xmm6, ecx, 1
+	xor	ecx, -1935334356
+	vpxord	xmm6, xmm6, dword ptr [rip + .LCPI0_18]{1to4}
+	vpaddd	xmm5, xmm5, xmm6
+	vprolvd	xmm5, xmm5, xmm0
+	xor	edx, -158062498
+	add	edx, ecx
+	rorx	ecx, edx, 3
+	vpxor	xmm6, xmm5, xmmword ptr [rip + .LCPI0_19]
+	vpshufd	xmm7, xmm5, 85                  # xmm7 = xmm5[1,1,1,1]
+	vpinsrd	xmm7, xmm7, ecx, 1
+	vpxord	xmm7, xmm7, dword ptr [rip + .LCPI0_20]{1to4}
+	vpaddd	xmm6, xmm6, xmm7
+	vprolvd	xmm6, xmm6, xmm0
+	xor	ecx, -238486900
+	vmovd	edx, xmm4
+	xor	edx, 1049849047
+	add	edx, ecx
+	rorx	ecx, edx, 3
+	vpbroadcastd	xmm4, ecx
+	vpxor	xmm4, xmm4, xmmword ptr [rip + .LCPI0_21]
+	vpermt2d	xmm5, xmm3, xmm6
+	vpxor	xmm5, xmm5, xmmword ptr [rip + .LCPI0_22]
+	vpaddd	xmm5, xmm4, xmm5
+	vpbroadcastq	xmm4, qword ptr [rip + .LCPI0_23] # xmm4 = [124554051611,124554051611]
+	vprolvd	xmm5, xmm5, xmm4
+	vpxor	xmm7, xmm5, xmmword ptr [rip + .LCPI0_24]
+	vpshufd	xmm8, xmm7, 85                  # xmm8 = xmm7[1,1,1,1]
+	vpaddd	xmm7, xmm8, xmm7
+	vmovd	ecx, xmm7
+	rorx	ecx, ecx, 5
+	vmovdqa	xmm7, xmm5
+	vpermt2d	xmm7, xmm2, xmm6
+	vpxor	xmm7, xmm7, xmmword ptr [rip + .LCPI0_25]
+	vpxor	xmm6, xmm6, xmmword ptr [rip + .LCPI0_26]
+	vpaddd	xmm6, xmm7, xmm6
+	vprolvd	xmm6, xmm6, xmm1
+	vmovd	edx, xmm6
+	vpxor	xmm7, xmm6, xmmword ptr [rip + .LCPI0_27]
+	vpermt2d	xmm6, xmm2, xmm5
+	vpxor	xmm5, xmm6, xmmword ptr [rip + .LCPI0_28]
+	vpaddd	xmm5, xmm7, xmm5
+	vprolvd	xmm5, xmm5, xmm1
+	vpshufd	xmm6, xmm5, 85                  # xmm6 = xmm5[1,1,1,1]
+	vpinsrd	xmm6, xmm6, ecx, 1
+	xor	ecx, 1199615334
+	xor	edx, -889141121
+	add	edx, ecx
+	vpxor	xmm7, xmm5, xmmword ptr [rip + .LCPI0_29]
+	vpxor	xmm6, xmm6, xmmword ptr [rip + .LCPI0_30]
+	rorx	ecx, edx, 5
+	vpaddd	xmm6, xmm7, xmm6
+	vprolvd	xmm6, xmm6, xmm1
+	vpshufd	xmm7, xmm6, 85                  # xmm7 = xmm6[1,1,1,1]
+	vpinsrd	xmm7, xmm7, ecx, 1
+	xor	ecx, -766164709
+	vmovd	edx, xmm5
+	xor	edx, -148315530
+	add	edx, ecx
+	vpxor	xmm5, xmm6, xmmword ptr [rip + .LCPI0_31]
+	rorx	ecx, edx, 5
+	vpxor	xmm7, xmm7, xmmword ptr [rip + .LCPI0_32]
+	vpaddd	xmm5, xmm5, xmm7
+	vprolvd	xmm5, xmm5, xmm1
+	vpbroadcastd	xmm7, ecx
+	vpxor	xmm7, xmm7, xmmword ptr [rip + .LCPI0_33]
+	vpermt2d	xmm6, xmm3, xmm5
+	vpxor	xmm6, xmm6, xmmword ptr [rip + .LCPI0_34]
+	vpaddd	xmm6, xmm7, xmm6
+	vpxor	xmm7, xmm5, xmmword ptr [rip + .LCPI0_35]
+	vpshufd	xmm8, xmm7, 85                  # xmm8 = xmm7[1,1,1,1]
+	vpaddd	xmm7, xmm8, xmm7
+	vmovd	ecx, xmm7
+	vprolvd	xmm6, xmm6, xmm0
+	vpxor	xmm7, xmm6, xmmword ptr [rip + .LCPI0_36]
+	vmovd	edx, xmm6
+	vpermt2d	xmm6, xmm2, xmm5
+	vpxord	xmm5, xmm6, dword ptr [rip + .LCPI0_37]{1to4}
+	rorx	ecx, ecx, 3
+	vpaddd	xmm5, xmm7, xmm5
+	vprolvd	xmm5, xmm5, xmm0
+	vpxor	xmm6, xmm5, xmmword ptr [rip + .LCPI0_38]
+	vpshufd	xmm7, xmm5, 85                  # xmm7 = xmm5[1,1,1,1]
+	vpinsrd	xmm7, xmm7, ecx, 1
+	xor	ecx, -215548264
+	vpxord	xmm7, xmm7, dword ptr [rip + .LCPI0_39]{1to4}
+	vpaddd	xmm6, xmm6, xmm7
+	vprolvd	xmm6, xmm6, xmm0
+	xor	edx, -1808355376
+	add	edx, ecx
+	rorx	ecx, edx, 3
+	vpxor	xmm7, xmm6, xmmword ptr [rip + .LCPI0_40]
+	vpshufd	xmm8, xmm6, 85                  # xmm8 = xmm6[1,1,1,1]
+	vpinsrd	xmm8, xmm8, ecx, 1
+	vpxord	xmm8, xmm8, dword ptr [rip + .LCPI0_41]{1to4}
+	vpaddd	xmm7, xmm8, xmm7
+	vprolvd	xmm7, xmm7, xmm0
+	xor	ecx, -544020639
+	vmovd	edx, xmm5
+	xor	edx, -1814075890
+	add	edx, ecx
+	rorx	ecx, edx, 3
+	vpbroadcastd	xmm5, ecx
+	vpxor	xmm5, xmm5, xmmword ptr [rip + .LCPI0_42]
+	vpermt2d	xmm6, xmm3, xmm7
+	vpxor	xmm3, xmm6, xmmword ptr [rip + .LCPI0_43]
+	shl	rsi, 32
+	vpaddd	xmm3, xmm5, xmm3
+	vprolvd	xmm3, xmm3, xmm4
+	vpxor	xmm4, xmm3, xmmword ptr [rip + .LCPI0_44]
+	vpshufd	xmm5, xmm4, 85                  # xmm5 = xmm4[1,1,1,1]
+	vpaddd	xmm4, xmm4, xmm5
+	vmovd	ecx, xmm4
+	rorx	ecx, ecx, 5
+	vmovdqa	xmm4, xmm3
+	vpermt2d	xmm4, xmm2, xmm7
+	vpxor	xmm4, xmm4, xmmword ptr [rip + .LCPI0_45]
+	vpxor	xmm5, xmm7, xmmword ptr [rip + .LCPI0_46]
+	vpaddd	xmm4, xmm4, xmm5
+	vprolvd	xmm4, xmm4, xmm1
+	vpxor	xmm5, xmm4, xmmword ptr [rip + .LCPI0_47]
+	vmovd	edx, xmm4
+	vpermt2d	xmm4, xmm2, xmm3
+	vpxor	xmm3, xmm4, xmmword ptr [rip + .LCPI0_48]
+	vpaddd	xmm3, xmm5, xmm3
+	vprolvd	xmm3, xmm3, xmm1
+	vpshufd	xmm4, xmm3, 85                  # xmm4 = xmm3[1,1,1,1]
+	vpinsrd	xmm4, xmm4, ecx, 1
+	xor	ecx, 520815624
+	xor	edx, -437755965
+	add	edx, ecx
+	rorx	ecx, edx, 5
+	vpxor	xmm5, xmm3, xmmword ptr [rip + .LCPI0_49]
+	vpxor	xmm4, xmm4, xmmword ptr [rip + .LCPI0_50]
+	vpaddd	xmm4, xmm5, xmm4
+	vprolvd	xmm4, xmm4, xmm1
+	vpshufd	xmm5, xmm4, 85                  # xmm5 = xmm4[1,1,1,1]
+	vpinsrd	xmm5, xmm5, ecx, 1
+	xor	ecx, -324498439
+	vmovd	edx, xmm3
+	xor	edx, 1373730486
+	add	edx, ecx
+	vpxor	xmm3, xmm4, xmmword ptr [rip + .LCPI0_51]
+	vpxor	xmm5, xmm5, xmmword ptr [rip + .LCPI0_52]
+	rorx	ecx, edx, 5
+	vpaddd	xmm3, xmm3, xmm5
+	vprolvd	xmm1, xmm3, xmm1
+	vpshufd	xmm3, xmm1, 85                  # xmm3 = xmm1[1,1,1,1]
+	vpinsrd	xmm3, xmm3, ecx, 1
+	vpxor	xmm3, xmm3, xmmword ptr [rip + .LCPI0_53]
+	vmovd	xmm5, ecx
+	vpunpckldq	xmm4, xmm5, xmm4        # xmm4 = xmm5[0],xmm4[0],xmm5[1],xmm4[1]
+	vpxord	xmm4, xmm4, dword ptr [rip + .LCPI0_54]{1to4}
+	vpaddd	xmm3, xmm3, xmm4
+	vpxor	xmm4, xmm1, xmmword ptr [rip + .LCPI0_55]
+	vpshufd	xmm5, xmm4, 85                  # xmm5 = xmm4[1,1,1,1]
+	vpaddd	xmm4, xmm4, xmm5
+	vmovd	ecx, xmm4
+	vprolvd	xmm3, xmm3, xmm0
+	vpxor	xmm4, xmm3, xmmword ptr [rip + .LCPI0_56]
+	vpermi2d	xmm2, xmm3, xmm1
+	vpxord	xmm1, xmm2, dword ptr [rip + .LCPI0_57]{1to4}
+	rorx	ecx, ecx, 3
+	vpaddd	xmm1, xmm4, xmm1
+	vprolvd	xmm1, xmm1, xmm0
+	vpxor	xmm2, xmm1, xmmword ptr [rip + .LCPI0_58]
+	vpshufd	xmm4, xmm1, 85                  # xmm4 = xmm1[1,1,1,1]
+	vpinsrd	xmm4, xmm4, ecx, 1
+	xor	ecx, 1908178372
+	vpxord	xmm4, xmm4, dword ptr [rip + .LCPI0_59]{1to4}
+	vpaddd	xmm2, xmm2, xmm4
+	vprolvd	xmm2, xmm2, xmm0
+	vmovd	edx, xmm3
+	xor	edx, 884053734
+	add	edx, ecx
+	vpxor	xmm3, xmm2, xmmword ptr [rip + .LCPI0_60]
+	rorx	ecx, edx, 3
+	vpshufd	xmm4, xmm2, 85                  # xmm4 = xmm2[1,1,1,1]
+	vpinsrd	xmm4, xmm4, ecx, 1
+	vpxord	xmm4, xmm4, dword ptr [rip + .LCPI0_61]{1to4}
+	or	rsi, rax
+	vpaddd	xmm3, xmm3, xmm4
+	vprolvd	xmm0, xmm3, xmm0
+	xor	ecx, -2112492491
+	vmovd	eax, xmm1
+	xor	eax, -814464733
+	add	eax, ecx
+	rorx	eax, eax, 3
+	vpxor	xmm1, xmm0, xmmword ptr [rip + .LCPI0_62]
+	vpshufd	xmm3, xmm1, 85                  # xmm3 = xmm1[1,1,1,1]
+	vpaddd	xmm1, xmm1, xmm3
+	vmovd	ecx, xmm1
+	rorx	ebp, ecx, 23
+	vpextrd	ecx, xmm0, 1
+	xor	ecx, 1534210138
+	mov	edx, eax
+	xor	edx, -546766179
+	add	edx, ecx
+	rorx	r14d, edx, 5
+	xor	eax, -884489825
+	vmovd	ecx, xmm2
+	xor	ecx, -546766179
+	add	ecx, eax
+	rorx	r15d, ecx, 3
+	bswap	ebp
+	bswap	r14d
+	mfence
+	bswap	r15d
+	vmovd	r12d, xmm0
+	rdtscp
+	bswap	r12d
+	shl	rdx, 32
+	or	rdx, rax
+	lfence
+	sub	rdx, rsi
+	mov	edi, offset .L.str
+	mov	rsi, rdx
+	mov	edx, ebp
+	mov	ecx, r14d
+	mov	r8d, r15d
+	mov	r9d, r12d
+	xor	eax, eax
+	call	printf
+	mov	edi, offset .L.str.1
+	mov	esi, offset .L.str.2
+	call	fopen
+	test	rax, rax
+	je	.LBB0_1
+# %bb.2:
+	mov	r13, rax
+	mov	esi, offset .L.str.4
+	mov	rdi, rax
+	mov	edx, ebp
+	mov	ecx, r14d
+	mov	r8d, r15d
+	mov	r9d, r12d
+	xor	eax, eax
+	call	fprintf
+	mov	rdi, r13
+	call	fclose
+	jmp	.LBB0_3
+.LBB0_1:
+	mov	edi, offset .Lstr
+	call	puts@PLT
+	mov	ebx, 1
+.LBB0_3:
+	mov	eax, ebx
+	add	rsp, 8
+	.cfi_def_cfa_offset 56
+	pop	rbx
+	.cfi_def_cfa_offset 48
+	pop	r12
+	.cfi_def_cfa_offset 40
+	pop	r13
+	.cfi_def_cfa_offset 32
+	pop	r14
+	.cfi_def_cfa_offset 24
+	pop	r15
+	.cfi_def_cfa_offset 16
+	pop	rbp
+	.cfi_def_cfa_offset 8
+	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object                  # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"%lu %u %u %u %u\n"
+	.size	.L.str, 17
+
+	.type	.L.str.1,@object                # @.str.1
+.L.str.1:
+	.asciz	"cipher.texts"
+	.size	.L.str.1, 13
+
+	.type	.L.str.2,@object                # @.str.2
+.L.str.2:
+	.asciz	"a"
+	.size	.L.str.2, 2
+
+	.type	.L.str.4,@object                # @.str.4
+.L.str.4:
+	.asciz	"%u\n%u\n%u\n%u\n"
+	.size	.L.str.4, 13
+
+	.type	.Lstr,@object                   # @str
+.Lstr:
+	.asciz	"Unable to open/create the file."
+	.size	.Lstr, 32
+
+	.ident	"clang version 16.0.6 (Fedora 16.0.6-3.fc38)"
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,13 +2,13 @@
 
 ./build.sh
 
-rm -rf ./env
+#rm -rf ./env
 
 #pip install virtualenv
 #python3-virtualenv or equivalent package needed!
-python3 -m venv env
+#python3 -m venv env
 source env/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
+#pip install --upgrade pip
+#pip install -r requirements.txt
 
 python3 benchmark.py

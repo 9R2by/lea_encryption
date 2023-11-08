68b1396e42ae05e3f4e14bbff3594967445a6abf
9R2by <37716834+9R2by@users.noreply.github.com>
2023-11-08 02:59:32 +0100

    clear asm folder

--- asm/build_asm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-clang main_with_benchmark.c -O3 -S -masm=intel -march=native -Wall -Wextra -Wpedantic -fno-asynchronous-unwind-tables -fno-dwarf2-cfi-asm -o main_with_benchmark.asm
-clang main.c -O3 -S -masm=intel -march=native -Wall -Wextra -Wpedantic -fno-asynchronous-unwind-tables -fno-dwarf2-cfi-asm -o main.asm
